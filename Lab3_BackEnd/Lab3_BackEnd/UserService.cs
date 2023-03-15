471d69cf734d3ed0c7da12819c975f151f197dc2
keszotrab <39887624+keszotrab@users.noreply.github.com>
2023-03-15 12:23:49 +0100

    Add files via upload

---
+++ Lab3_BackEnd/Lab3_BackEnd/UserService.cs
@@ -0,0 +1,30 @@
+﻿namespace Lab3_BackEnd
+{
+    public class  UserService
+    {
+        Dictionary<string, string> Users = new Dictionary<string, string>();
+
+
+        public void Add(string connectionId, string username) 
+        {
+
+            Users.Add(username, connectionId);
+        }
+        
+        public void RemoveByName(string username) 
+        {
+            Users.Remove(username);
+        }
+
+        public string GetConnectioIdByName(string username) 
+        {
+            return Users[username];
+        }
+        public IEnumerable<(string ConnectionId, string Username)> GetAll() 
+        {
+            return Users.Select(e => new {ConnectionId = e.Value, Username = e.Key }).AsEnumerable();
+            
+        }
+
+    }
+}
